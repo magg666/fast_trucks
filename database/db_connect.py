067fbf0032f7b8dac7164965ed9a91c39e9d122a
magg666 <mw23127@gmail.com>
2021-11-24 04:41:59 +0100

    added variable port

--- database/db_connect.py
+++ database/db_connect.py
@@ -7,7 +7,6 @@ load_dotenv()
 
 def connect():
     host = os.environ.get("HOST")
-    port = os.environ.get("PORT")
     user = os.environ.get("USER")
     password = os.environ.get("PASSWORD")
     db = os.environ.get("DBNAME")
@@ -16,7 +15,7 @@ def connect():
 
     if env_variables_defined:
         return pymysql.connect(host=host,
-                               port=port,
+                               port=25060,
                                user=user,
                                password=password,
                                database=db,
