d3a8e59e8225e1e0e5838cf44499f9c7efc0c2b6
magg666 <mw23127@gmail.com>
2021-11-25 19:21:47 +0100

    deployment experiment

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
+from flask_restful import Api, Resource, reqparse
+# from flask_cors import CORS
+from api.api import HelloApiHandler
+
 from app import find_countries_and_path
 from validator import is_shortcut_valid
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+# CORS(app)
+api2 = Api(app)
 
 # handling basics errors
 @app.errorhandler(405)
@@ -15,6 +20,9 @@ def method_not_allowed(error):
 def not_found(error):
     return {'error': 'You should not go there...'}
 
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder, 'index.html')
 
 # define route to api services. Allowed method - only GET
 @app.route('/api/<string:end_country>', methods=["GET"])
@@ -25,6 +33,8 @@ def api(end_country):
     else:
         return {'error': "Destination must be three-letter shortcut"}
 
+
+api2.add_resource(HelloApiHandler, '/flask/hello')
 
 if __name__ == '__main__':
     app.run()
